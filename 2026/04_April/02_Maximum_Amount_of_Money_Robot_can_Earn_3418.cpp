4a8a0386eb1d6055300d076d02c610d93828d85e
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-04-02 22:19:05 +0530

    Day 2 : Maximum Amount of money Robot can Earn(3418)

---
+++ 2026/04_April/02_Maximum_Amount_of_Money_Robot_can_Earn_3418.cpp
@@ -0,0 +1,33 @@
+// Problem : Maximum Amount of money Robot can Earn(LC 3418)
+// Approach : Dynamic Programing
+// Time Complexity : O(m * n)
+
+#include <algorithm>
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int n = coins[0].size();
+        vector<vector<int>> dp(n + 1, vector<int>(3, INT_MIN / 2));
+
+        for (int i = 0; i < 3; i++) {
+            dp[1][i] = 0;
+        }
+        for (auto& row : coins) {
+            for (int j = 1; j <= n; j++) {
+                int x = row[j - 1];
+                dp[j][2] = max(
+                    {dp[j - 1][2] + x, dp[j][2] + x, dp[j - 1][1], dp[j][1]});
+                dp[j][1] = max(
+                    {dp[j - 1][1] + x, dp[j][1] + x, dp[j - 1][0], dp[j][0]});
+                dp[j][0] = max(dp[j - 1][0], dp[j][0]) + x;
+            }
+        }
+
+        return dp[n][2];
+    }
+};
