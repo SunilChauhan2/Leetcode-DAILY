5985e204f9e2f60cc569ef431aad50a9ad3dbc49
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-04-04 12:01:26 +0530

    Day 4: Decode the Slanted Ciphertext(LC 2075)

---
+++ 2026/04_April/04_Decode_the_Slanted_Ciphertext.cpp
@@ -0,0 +1,30 @@
+// Problem : (LC 2075)Decode the Slanted Ciphertext
+// Approach : Matrix Simulation + Diagonal Traversal
+// Time Complexity : O(n)
+
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    string decodeCiphertext(string encodedText, int rows) {
+        int len = encodedText.length();
+        if (len == 0) return "";
+        
+        int cols = len / rows;
+        string ans = "";
+        for (int j = 0; j < cols; j++) {
+            for (int r = 0, c = j; r < rows && c < cols; r++, c++) {
+                ans.push_back(encodedText[r * cols + c]);
+            }
+        }
+        
+        while (!ans.empty() && ans.back() == ' ') {
+            ans.pop_back();
+        }
+        
+        return ans;
+    }
+};
