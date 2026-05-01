bcb8a8770b7b66b9164517fee663d779ce121285
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-05-01 22:12:51 +0530

    Day 1: (LC 396)Rotate Function

---
+++ 2026/05_May/01_Rotate_Function.cpp
@@ -0,0 +1,24 @@
+// Problem : (LC 396)Rotate Function
+// Approach : Prefix sum / Simulation
+// Time Complexity : O(N)
+
+#include<vector>
+#include<numeric>
+using namespace std;
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int f = 0, n = nums.size();
+        int numSum = accumulate(nums.begin(), nums.end(), 0);
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
+        }
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = max(res, f);
+        }
+        return res;
+    }
+};
