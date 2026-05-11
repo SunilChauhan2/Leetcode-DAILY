12d02d621403b2baeb9d2c98798ab63684265acb
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-05-11 22:28:39 +0530

    Day 11: (LC 2553)Seperate the digits in an array

---
+++ 05_May/11_Seperate_the_Digits_in_an_Array.cpp
@@ -0,0 +1,24 @@
+// Problem : (LC 2553)Seperate the digits in an array
+// Approach : Array / Simulation
+// Time Complexity : O(nlogm)
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> res;
+        for (auto x : nums) {
+            vector<int> tmp;
+            while (x > 0) {
+                tmp.push_back(x % 10);
+                x /= 10;
+            }
+            for (int i = tmp.size() - 1; i >= 0; i--) {
+                res.push_back(tmp[i]);
+            }
+        }
+        return res;
+    }
+};
