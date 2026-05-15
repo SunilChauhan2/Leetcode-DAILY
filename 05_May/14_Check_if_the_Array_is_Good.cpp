e2f71f87820c0a4cc853dd4ab7de9bf5a32e128b
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-05-15 22:47:45 +0530

    new

---
+++ 05_May/14_Check_if_the_Array_is_Good.cpp
@@ -0,0 +1,27 @@
+// Problem : (LC 2784)Check if array is good
+// Approach : Array / Counting / Pigeonhole Principle
+// Time Complexity : O(N)
+
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> count(n, 0);
+        for (int a : nums) {
+            if (a >= n) {
+                return false;
+            }
+            if (a < n - 1 && count[a] > 0) {
+                return false;
+            }
+            if (a == n - 1 && count[a] > 1) {
+                return false;
+            }
+            count[a]++;
+        }
+        return true;
+    }
+};
