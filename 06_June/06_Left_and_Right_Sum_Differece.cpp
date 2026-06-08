491e53b40611620098728924abee2fad78cfb76d
SunilChauhan2 <sunilchauhan27c@gmail.com>
2026-06-08 22:58:32 +0530

    Day 6: (LC 2574)Left and Right sum difference

---
+++ 06_June/06_Left_and_Right_Sum_Differece.cpp
@@ -0,0 +1,28 @@
+// Problem : (LC 2574)Left and Right sum difference
+// Approach : Prefix Sum
+// Time Complexity : O(N)
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        int leftSum = 0;
+        for (int i = 0; i < n; ++i) {
+            ans[i] = leftSum;
+            leftSum += nums[i];
+        }
+
+        int rightSum = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            ans[i] = abs(ans[i] - rightSum);
+            rightSum += nums[i];
+        }
+
+        return ans;
+    }
+};
